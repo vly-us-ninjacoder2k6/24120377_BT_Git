ea9487089fdd037b273ec66d00a23c063c7b8372
DoranUwU <dorankongu@gmail.com>
2026-03-30 19:15:12 +0700

    hello

---
+++ main.py
@@ -0,0 +1,12 @@
+<<<<<<< HEAD
+def isPrime(n):
+    if n < 2: return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+            
+=======
+print('Hello Github')
+>>>>>>> 98a9087df4dcda126b85ea69a7b16695d1c145a4
